c4879933c22c02088357ec3524566e11524a0b3b
Jorge Olave <jaolave@gmail.com>
2025-07-21 12:36:50 -0500

    Add API

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'API Laravel',
+        'version' => '1.0.0',
+        'endpoints' => '/api/*'
+    ]);
 });
